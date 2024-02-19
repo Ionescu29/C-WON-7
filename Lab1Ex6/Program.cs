36ac9033caee3b8f647a7d25d033018d72cb6463
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-02-19 18:56:06 +0200

    Add files via upload
    Aici sunt exercitiile. Sunt cateva chestii pe care nu am inteles exact cum trebuie sa le folosesc si nu am inteles modul in care ar putea fi facute ultimele exercitii

---
+++ Lab1Ex6/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+//impar
+//in cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”.
+
+int n = 23;
+// nu am reusit sa gasesc o formul care sa ma ajute sa demonstrez paritatea sau neparitatea numarului ales
