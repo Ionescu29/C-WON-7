9dfa2b4f8f8ff664b32b553ad1f76bd00c03092a
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-02-19 18:56:55 +0200

    Add files via upload

---
+++ Lab1Ex7/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica
+//daca x este divizibil cu y. 
+//In cazul in care x este divizibil cu y, programul va afisa “divizibil” iar in caz contrar,
+//“indivizibil”.
+int x = 20;
+int y = 10;
+int u = 0;
+//u fiind ultima cifra a numarului 20
+if (u = 0) ;
+{
+    Console.WriteLine("divizibil");
+}
+if (u > 0) ;
+{
+    Console.WriteLine("Indivizibil");
+}
+// Am folosit criteriul de diviziblitate cu 10
