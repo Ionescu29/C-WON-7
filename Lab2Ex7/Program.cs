e4a1700c2391beb179e6dfc750172da83b01445f
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-02-26 18:53:44 +0200

    Add files via upload

---
+++ Lab2Ex7/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Se citesc trei numere de la
+//tastatura, x, y, z. Scrieti un program care va afisa cele trei valori in ordine
+//descrescatoare.
+int x = 7;
+int y = 2;
+int z = 3;
+if(x<y && x<z
+{
+    Console.WriteLine("ordinea este" + x, y, z);
+}
+else if(y<x && y<z)
+{
+    Console.WritelLine("ordinea este" + y, z, x);
+}
+else (z<x && z<y)
+{
+    Console.WriteLine("ordinea este" + z < x < y);
+}
