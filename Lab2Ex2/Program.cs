ae3128bbcffff9295a692386feb0970c57559039
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-02-26 18:54:35 +0200

    Add files via upload

---
+++ Lab2Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Scrieti un program care va determina daca un numar negativ citit de
+//la tastatura este divizibil cu 2 si nu este divizibil cu 6
+Console.ReadLine();
+int -x;
+if (-x%2!=0)
+{
+    Console.WriteLine("este dizibil cu 6 si nu este dizivbil cu 2");
+}
+else (-x%2=0)
+{
+    Console.WriteLine("Este divizibil cu 2 si nu este divizibil cu 6");
+}
